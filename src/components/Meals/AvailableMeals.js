5aaf8c8c1cbce4dea980ae0b629d0950046a298c
thinhphungdev <phungthinh0946519356@gmail.com>
2021-07-08 16:37:06 +0700

    connect to the backend

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,50 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from '../UI/Card.js';
 import MealItem from './MealItem/MealItem.js';
 
 import classes from './AvailableMeals.module.css';
 
-const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-];
 
 const AvailableMeals = () => {
-    const mealLists = DUMMY_MEALS.map(meal => <MealItem 
-      name={meal.name}
-      description={meal.description}
-      price={meal.price}
-      key={meal.id}
-      id={meal.id}
-    />)
+  const [meals, setMeals] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
-    return <section className={classes.meals}>
-      <Card>
-          {mealLists}
-      </Card>
+  useEffect(() => {
+    const fetchMeals = async () => {
+      try {
+        const res = await fetch('https://react-http-e25c6-default-rtdb.firebaseio.com/meals.json')
+        const resData = await res.json();
+
+        console.log('res data', resData)
+
+        const loadedMeals = [];
+
+        for (const key in resData) {
+          loadedMeals.push({
+            id: key,
+            name: resData[key].name,
+            description: resData[key].description,
+            price: resData[key].price
+          })
+        }
+
+        setMeals(loadedMeals)
+        setIsLoading(false)
+      } catch (err) {
+        console.error(err.message)
+      }
+    }
+
+    fetchMeals()
+  }, [])
+
+  if (isLoading) {
+    return <section className={classes['meals-loading']}> 
+      <p>Loading...</p>
     </section>
+  }
+
+  const mealLists = meals.map(meal => <MealItem
+    name={meal.name}
+    description={meal.description}
+    price={meal.price}
+    key={meal.id}
+    id={meal.id}
+  />)
+
+  return <section className={classes.meals}>
+    <Card>
+      <ul>
+        {mealLists}
+      </ul>
+    </Card>
+  </section>
 }
 
 export default AvailableMeals;
